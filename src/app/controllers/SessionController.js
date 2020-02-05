e63e1d9709b3ee0e0c483b0036335c156cceb8fe
kadson jader martins ferreira <kadsonj@MacBook-Pro-de-kadson.local>
2020-02-05 12:29:04 -0300

    adicionando avatar do usuario

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -6,14 +6,16 @@ import User from '../models/User';
 
 class SessionController {
   async store(req, res) {
-      const schema = Yup.object().shape({
-        email: Yup.string().email().required(),
-        password: Yup.string().required()
-      });
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
 
-      if (!(await schema.isValid(req.body))){
-        return res.status(400).json({ error: 'erro de validação' })
-      }
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'erro de validação' });
+    }
 
     const { email, password } = req.body;
 
